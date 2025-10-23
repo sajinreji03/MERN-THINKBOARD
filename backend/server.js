188d26c681b40c4866e294ca88c2b86d241f4ca5
sajinreji03 <sajinreji9@gmail.com>
2025-10-24 02:27:55 +0530

    Updationsto backend code

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 import express from "express"
-// const express = require("express")
+import notesRoutes from "../routes/notesRoutes.js"
+
 const app = express()
 
-app.get("/api/notes", (req, res) =>{
-    res.send("You got 5 notes");
-})
+app.use("/api/notes", notesRoutes);
+
+
+// app.get("/api/notes", (req, res) =>{
+//     res.status(200).send("You got 20 notes");
+// });
+
+// app.post("/api/notes", (req, res) =>{
+//     res.status(201).json({message:"Note created successfully"});
+// })
+
+// app.put("/api/notes/:id", (req, res) =>{
+//     res.status(200).json({message:"Note updated successfully"});
+// })
+
+// app.delete("/api/notes/:id", (req, res) =>{
+//     res.status(200).json({message:"Note deleted successfully"});
+// })
 
 app.listen(5001, () =>{
     console.log("Server started on port: 5001");
