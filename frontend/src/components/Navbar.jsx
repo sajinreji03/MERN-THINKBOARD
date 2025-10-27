52f6a534e64473120ef941aeb1298a586ff9a859
sajinreji03 <sajinreji9@gmail.com>
2025-10-27 17:44:17 +0530

    set up the frontend pages

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { PlusIcon } from 'lucide-react'
+import React from 'react'
+import { Link } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <header className='border-b border-base-content/10 bg-base-300 '>
+      <div className='max-w-6xl p-4 mx-auto '>
+       <div className='flex items-center justify-between'>
+        <h1 className='font-mono text-3xl font-bold tracking-tighter text-primary'>
+          ThinkBoard </h1>
+        <div className='flex items-center gap-4'>
+            <Link to={"/create"} className='btn btn-primary'>
+            <PlusIcon className='size-5' />
+            <span>New Note</span>
+            </Link>
+        </div>
+       </div>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+import daisyui from "daisyui";
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+   daisyui: {
+    themes: ["night", "forest" ,"coffee" ],
+  },
+};
